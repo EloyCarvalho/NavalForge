dfb5f66e5df5de6a4196122fb303496257211fa0
EloyCarvalho <eloy.engenheiro@gmail.com>
2026-05-03 02:17:32 -0300

    Expose validation database helpers

--- src/navalforge/validation/__init__.py
+++ src/navalforge/validation/__init__.py
@@ -1,5 +1,20 @@
 """Validation helpers for NavalForge."""
 
 from .cases import ValidationCase, summarize_validation
+from .database import (
+    ReferenceSource,
+    ValidationCaseRecord,
+    load_reference_sources,
+    load_validation_case_records,
+    summarize_validation_database,
+)
 
-__all__ = ["ValidationCase", "summarize_validation"]
+__all__ = [
+    "ValidationCase",
+    "summarize_validation",
+    "ReferenceSource",
+    "ValidationCaseRecord",
+    "load_reference_sources",
+    "load_validation_case_records",
+    "summarize_validation_database",
+]
