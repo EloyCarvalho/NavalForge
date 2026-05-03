291d3aba72842f36709fc77105ab43b417b193cc
EloyCarvalho <eloy.engenheiro@gmail.com>
2026-05-03 02:06:43 -0300

    Add reporting package

---
+++ src/navalforge/reporting/__init__.py
@@ -0,0 +1,5 @@
+"""Reporting helpers for NavalForge."""
+
+from .technical_report import technical_result_to_markdown, write_technical_report
+
+__all__ = ["technical_result_to_markdown", "write_technical_report"]
