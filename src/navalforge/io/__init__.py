e7922aa4136bba33a2fc218ecdc6108eacb4629a
EloyCarvalho <eloy.engenheiro@gmail.com>
2026-05-03 00:59:36 -0300

    Expose CSV section IO helpers

--- src/navalforge/io/__init__.py
+++ src/navalforge/io/__init__.py
@@ -1,1 +1,5 @@
-"""Input/output helpers."""
+"""Input/output helpers for NavalForge."""
+
+from .sections_csv import load_sectional_hull_from_csv, write_sectional_hull_to_csv
+
+__all__ = ["load_sectional_hull_from_csv", "write_sectional_hull_to_csv"]
