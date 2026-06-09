f9ced31113e3c5cdde02a6df7b0f70bbc4774679
EloyCarvalho <eloy.engenheiro@gmail.com>
2026-06-09 18:55:48 -0300

    Add Similar Vessels R0 package init

---
+++ tools/navalforge_similar_vessels_r0/navalforge_benchmark/__init__.py
@@ -0,0 +1,8 @@
+"""NavalForge Similar Vessels R0."""
+
+from .benchmark import NavalForgeBenchmark
+from .models import ProjectTarget
+from .similarity import SimilarityConfig
+
+__all__ = ["NavalForgeBenchmark", "ProjectTarget", "SimilarityConfig"]
+__version__ = "0.1.0-r0"
